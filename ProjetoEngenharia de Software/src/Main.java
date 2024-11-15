759f67e6ab41e93cf85d2fc111532e7ae1531479
Pedro Sampaio <pamms@ua.pt>
2024-11-15 23:29:46 +0000

    Codigo inicial

---
+++ ProjetoEngenharia de Software/src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(obterResultado(210));
+    }
+
+    public static String obterResultado(int numero){
+        String resultado = "";
+
+        if (numero % 3 == 0){
+            resultado +="Pim";
+        }
+        if (numero % 5 == 0){
+            resultado +="Pam";
+        }
+        if (numero % 7 == 0){
+            resultado +="Pum";
+        }
+
+        String numeroEmString = Integer.toString(numero);
+        for (char digito : numeroEmString.toCharArray()){
+            if (digito == '3'){
+                resultado +="Pim";
+            }else if (digito == '5'){
+                resultado +="Pam";
+            }else if (digito == '7'){
+                resultado +="Pum";
+            }
+        }
+
+        if (resultado.isEmpty()){
+            return numeroEmString;
+        }else {
+            return resultado;
+        }
+    }
+    
+}
